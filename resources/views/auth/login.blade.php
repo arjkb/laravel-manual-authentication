7149a94dfcf899bdb54a17c07f02270e26d4998b
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2022-01-11 15:28:13 +0530

    create login html page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,19 @@
+@extends('base')
+
+@section('content')
+
+<h2 class="mt-5 mb-3">Login</h2>
+<form method="POST" action="login">
+    @csrf
+    <div class="mb-3">
+        <label for="username" class="form-label">Username</label>
+        <input type="text" class="form-control" id="username" name="username">
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" name="password">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Login</button>
+</form>
+@endsection
